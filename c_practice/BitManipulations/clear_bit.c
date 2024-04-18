be5f74cfbab785cf67a2d01903e53c3ae492d31d
pavan kumar c <pavankumar.jul1@gmail.com>
2024-04-18 23:55:58 +0530

    added bit manipulation repo

--- c_practice/BitManipulations/clear_bit.c
+++ c_practice/BitManipulations/clear_bit.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a=10;
-    /*below we clearing a 1 bit and assiging 
+    /*below we are clearing the first(1) bit and assiging 
     result to variable 'a' itself*/
     a=a&~(1<<1); //logic to clear a bit!
     printf("%d",a); 
